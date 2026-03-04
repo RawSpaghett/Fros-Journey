6bdcb43dbfb002892a9975b4b2f7e4c0d3157f51
RawSpaghett <rawspaghetti05@gmail.com>
2026-03-04 16:40:47 -0500

    some camera logic

---
+++ Assets/Scripts/Camera/CameraCollider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraCollider : MonoBehaviour
+{
+    private Camera WorldCam;
+    public Vector3 TargetPosition;
+
+    void Start()
+    {
+       //grab camera
+    }
+
+   void OnCollisionEnter2D(Collision2D collider)
+   {
+      //move camera 
+   }
+}
---
+++ Assets/Scripts/Camera/WorldCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+//uses colliders to determine screen changes, getcomponent, allows for unique rooms sizes, NOT FEASIBLE FOR INFINITE GENERATION
+public class WorldCamera : MonoBehaviour
+{
+    public Vector3 CurrentPosition; //define where it needs to go
+    
+    void Start()
+    {
+        CurrentPosition = transform.position; //set it to current position
+    }
+
+    void MoveTo(Vector3 TargetPosition) //to be called by collision script, snaps into place
+    {
+        transform.position = TargetPosition;
+        CurrentPosition = TargetPosition;
+    }
+   
+}
