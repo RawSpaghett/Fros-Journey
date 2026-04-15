5b19d9010aeb9550761be120c05392112b02321c
cecil616 <bluehumanspider@yahoo.com>
2026-04-14 23:57:40 -0400

    Add files via upload

--- Assets/Scripts/Player/PlayerController.OnTrigger.cs
+++ Assets/Scripts/Player/PlayerController.OnTrigger.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public partial class PlayerController: MonoBehaviour
 {
-     private void OnTriggerEnter2D(Collider2D other) //when entering the water
+    private void OnTriggerEnter(Collider other) //when entering the water
     {
         if (other.CompareTag("Water"))
         {
@@ -11,21 +11,19 @@ public partial class PlayerController: MonoBehaviour
             Swimming();
             Debug.Log("<Color=blue> WATER </color>");
         }
+
+        if (other.CompareTag("Egg")) //kaitlyn
+        {
+            Destroy(other.gameObject);
+            em.eggCount++;
+        }
         //waterCheck water gameobject if true isSwimming
         //private void isSwimming
         //if isSwimming gravity -2ish player should freely move x,y
         //still allow player to jump but at a static rate
     }
-    private void OnTriggerEnter(Collider other)
-    {
-    if (other.gameObject.CompareTag("Egg")) //kaitlyn
-        {
-            Destroy(other.gameObject);
-            em.eggCount++;
-        }
-    }
 
-     private void OnTriggerExit2D(Collider2D other) //when leaving the water
+    private void OnTriggerExit(Collider other) //when leaving the water
     {
         if (other.CompareTag("Water"))
         {
