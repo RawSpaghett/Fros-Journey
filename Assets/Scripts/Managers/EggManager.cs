6f5fc310af9d72804528e9fe866d77e1bcbdee44
cecil616 <bluehumanspider@yahoo.com>
2026-04-26 03:45:56 -0400

    Add files via upload

--- Assets/Scripts/Managers/EggManager.cs
+++ Assets/Scripts/Managers/EggManager.cs
@@ -1,20 +1,73 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EggManager : MonoBehaviour
 {
+    public static EggManager instance;
     public int eggCount;
+    public int totalEggs;
     public Text eggText;
+    public int lotusCount;
+    public int totalLotus;
+
+    public TMP_Text finishText;
     public GameObject finish;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Start()
     {
-        eggText.text = ": " + eggCount.ToString();
+        totalEggs = GameObject.FindGameObjectsWithTag("Egg").Length;
+        totalLotus = GameObject.FindGameObjectsWithTag("Lotus").Length;
+
+        UpdateUI(); // initialize UI at start
+    }
+
+    // This replaces manually changing eggCount
+    public void AddEgg()
+    {
+        eggCount++;
+        UpdateUI();
+
+        if (eggCount >= totalEggs)
+        {
+            LevelComplete();
+        }
+    }
+
+    public void AddLotus()
+    {
+        lotusCount++;
+        UpdateUI();
+    }
+
+
+    // This replaces your Update() method
+    void UpdateUI()
+    {
+        if (eggText != null)
+            eggText.text = eggCount + " / " + totalEggs + "  Lotus: " + lotusCount + "/" + totalLotus; ;
+    }
+
+
+    void LevelComplete()
+    {
+        if (finish != null)
+            finish.SetActive(true);
+
+        if (finishText != null)
+        {
+            finishText.text =
+                "Eggs: " + eggCount + " / " + totalEggs +
+                "\nLotus: " + lotusCount + " / " + totalLotus;
+        }
+
     }
 }
